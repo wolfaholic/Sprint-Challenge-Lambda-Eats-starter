ad48af046510216cbe507a213d3a8c7c996320d0
Wolfaholic <melissa.mackie@live.com>
2020-06-14 19:32:16 -0700

    header and header button complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Button, Navbar, Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText, CardGroup, CardColumns } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <Navbar color='danger'>
+      <h1 style={{color: 'white'}}>Lambda Eats</h1>
+      <Link to={'/'}>
+        <Button color='light' >
+          Home
+        </Button>
+        <Button color='light' >
+          Help
+        </Button>
+      </Link>
+    </Navbar>
+    <Card>
+      <CardImg src={require('./Assets/Pizza.jpg')} />
+      <Button color="danger" style={{position: 'absoloute', width:'10%', left:'50%', top:'50%'}}>
+      Pizza?
+      </Button>
+    </Card>
+
+
+
+     </>
   );
 };
 export default App;
